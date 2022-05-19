32aacb164f2ca2eb45ab7a74729c0b8f289b3098
veronicaliljegren <liljegrenveronica@gmail.com>
2022-05-19 10:06:50 +0200

    added functions for programme page

--- programSida/programSida.js
+++ programSida/programSida.js
@@ -1,11 +1,52 @@
 "use strict"
-
-function renderProgrammeById (programmeId) {
+//funktion som ska lägga till programmets namn i headern
+function renderProgrammeById(programmeId) {
     for(let programme of DB.PROGRAMMES) {
         if(programme.id == programmeId) {
+            let programmeName = document.getElementById("course");
+            programmeName.classList.add("content");
+            programmeName.innerHTML = `
+            <p class ="course-name">${programme.name}
+            `;
 
-            console.log(`${programme.name}`);
+            // // console.log(`${programme.name}`);
+            // let programmeImage = document.getElementById("wrapper");
+            //vi måste lägga in bilderna i databasen! 
+            // programmeImage.style.backgroundImage =url(/bilder/design.webp);
         }
     }
-
 }
+
+//funktion som lägger till information om programmet
+function renderInfoProgramme(programmeId){
+    for( let programme of DB.PROGRAMMES){
+        let programmeInfoDiv = document.createElement("div");
+        programmeInfoDiv.id = "programme-info";
+        
+        let containerDiv = document.getElementById("container");
+        containerDiv.appendChild(programmeInfoDiv);
+
+        programmeInfoDiv.innerHTML = `
+        <p>${programme.exchangeStudents}</p>
+        <p>${getUniversityById(programmeId)}</p>
+       
+        `;
+       return programmeInfoDiv;
+    }
+    
+}
+
+function getUniversityById (programmeId) {
+    let universityName = UNIVERSITIES.find(university => {
+        if (university.id == PROGRAMMES.programmeId.universityID) {
+            return true;
+        }
+    });
+    return universityName.name;
+ }
+
+// function renderProgrammeNamesById(){
+//     for 
+// }
+
+renderProgrammeById(8);
