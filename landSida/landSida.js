f203f39779458134cfe6d5f90c47a0f1c0532e49
Damianmilczarek5 <97959489+Damianmilczarek5@users.noreply.github.com>
2022-05-19 10:43:55 +0200

    made sort functions

--- landSida/landSida.js
+++ landSida/landSida.js
@@ -1,5 +1,26 @@
 "use strict";
 
+DB.CITIES.sort(function(a, b) {
+    if (a.name > b.name) {
+    return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+      }
+    return 0;
+      });
+
+      DB.COUNTRIES.sort(function(a, b) {
+        if (a.name > b.name) {
+        return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+          }
+        return 0;
+          });
+     
+
 function renderCountry (id) {
     let div = document.createElement("div");
      div.id = "land-box";
@@ -41,8 +62,8 @@ function renderCityInCountry (countryId) {
             for ( let city of DB.CITIES) {
                 if (city.countryID == country.id) {
                     let cityDiv = `
-                    <div class="stad-box" onClick="cityClick(${city.id})">
-                        <p>${city.name}</p>
+                    <div class="stad-box"  onClick="cityClick(${city.id})">
+                        <div id="hej"><p>${city.name}</p> </div>
                     </div>
                     `;
                     cityArray.push(cityDiv);
@@ -51,11 +72,25 @@ function renderCityInCountry (countryId) {
             } 
         }
     }
-
-    
     return cityArray.join("");
 }
-//funktion som visar bilder bakom länder
+
+function cityImagee (countryId) {
+    for ( let country of DB.COUNTRIES ) {
+        if (countryId == country.id ) {
+            for ( let city of DB.CITIES) {
+                if (city.countryID == country.id) {
+                    let cityImage = document.getElementById("hej");
+             cityImage.style.backgroundImage=`url(../databasen/Images/${city.imagesNormal[0]})`;
+
+                  
+                }
+            } 
+        }
+    }
+}
+
+
 function citiesImages(cityArray){
     for (let city of cityArray){
         if(city.id == cityArray.id){
@@ -66,22 +101,14 @@ function citiesImages(cityArray){
     }
 }
 
-//sortera länderna i bokstavsordning
-// COUNTRIES.sort(function(a, b){
-//     if (a.name > b.name) {
-//         return 1;
-//     }
-//     if (a.name < b.name){
-//         -1;
-//     }
-//     return 0;
-// });
-citiesImages(DB.CITIES);
+function cityClick (cityId){
+window.sessionStorage.setItem("stad", cityId);
+window.location.href = "../stadsida/stadSida.html";
+}
+
+
+
+//funktion som visar bilder bakom länder
+
 renderCountries(DB.COUNTRIES);
 
-
-
-function cityClick (cityId){
-    window.sessionStorage.setItem("stad", cityId);
-    window.location.href = "../stadsida/stadSida.html";
-    }
